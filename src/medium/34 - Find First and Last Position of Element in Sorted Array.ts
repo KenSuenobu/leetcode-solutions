0a4a23ff0c712212743f02a3f2d930d90749326b
Ken Suenobu <ksuenobu@fastmail.com>
2022-08-24 15:49:30 -0600

    chore: added first/last occurrence challenge

---
+++ src/medium/34 - Find First and Last Position of Element in Sorted Array.ts
@@ -0,0 +1,28 @@
+/**
+ * Searches for the first and last occurrence of a target against a numbered array.
+ * 
+ * @param nums The array of numbers to search against
+ * @param target The target to search for
+ * @returns The first and last occurrence of target's position in the `nums` array, or -1 if not found.
+ */
+function searchRange(nums: number[], target: number): number[] {
+    if (nums.length === 0) {
+        return [-1, -1];
+    }
+    
+    let first = -1;
+    let last = -1;
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            if (first === -1) {
+                first = last = i;
+                continue;
+            }
+            
+            last = i;
+        }
+    }
+    
+    return [first, last];
+};
