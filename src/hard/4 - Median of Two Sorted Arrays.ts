2d0fa2836d70f335e031c6f8a46ba6c753ef4c38
Kenji Suenobu <kenji@Kenjis-MacBook-Pro.local>
2022-08-24 09:41:01 -0600

    chore: added solution to problem 4

---
+++ src/hard/4 - Median of Two Sorted Arrays.ts
@@ -0,0 +1,16 @@
+/**
+ * Takes a list of numbers in any order, and finds the median of the values in the array.
+ * 
+ * @param nums1 First array of numbers
+ * @param nums2 Second array of numbers
+ * @returns 
+ */
+function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
+    const combined = [...nums1, ...nums2].sort((a, b) => a - b);
+    
+    // Shortcut: If an array is oddly sized, the median will always be the single digit in the
+    // center.  Otherwise, we take the median, start one lower than the offset (0 offset), and
+    // add the number directly next to it, divide by two, and return the result.
+    return (combined.length % 2 == 1) ? combined[Math.round(combined.length / 2) - 1] :
+           (combined[Math.round(combined.length / 2) - 1] + combined[Math.round(combined.length / 2)]) / 2;
+};
